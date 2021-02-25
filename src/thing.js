7cb76ca422476461bea1f068316f1e8d3c73c615
Crawford Vining <crawfordvining@fm-cravin2-mac.home>
2021-02-24 19:36:48 -0800

    tried to get random url working; setstate is working but url is not

--- src/thing.js
+++ src/thing.js
@@ -1,29 +1,31 @@
 import React, {useState, useEffect} from 'react'
 
 export default function Thing() {
-    const [thingNum, setThingNum] = useState(1)
+    const [thingNum, setThingNum] = useState(4)
     const [thing, setThing] = useState({})
 
-    let number = Math.floor(Math.random() * 11)
-    console.log(number)
-
-    // setThingNum[thingNum(Math.floor(Math.random() * 11))]
-    // return thingNum
-    // console.log(thingNum)
-
-    useEffect(() => {
+    function updateNum() {
+        setThingNum(Math.floor(Math.random() * 11)) 
+    } 
+    
+    useEffect(() => { 
+        
+        console.log(thingNum)    
+    
         fetch(`https://swapi.dev/api/people/${thingNum}/?format=json`)
         .then(response => response.json())
         .then(json => {
-            // console.log('name of the day', json)
             setThing(json)
+            console.log(thing)
         })
+          
     }, [])
 
     return(
         <div>
             <h2>testing time! Here is the number {thingNum}</h2>
             <p><strong>Name:</strong> {thing.name}</p>
+            <button onClick={updateNum}>Random</button>
         </div>
     )
 }
